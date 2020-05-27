399cd299afd9fc24a8037bcf1f52e835b096dafd
Your_Bro <pontiy.pilot@gmail.com>
2020-05-27 09:29:49 +0400

    визуальные правки

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   let params = {};
+  const mainBtn = document.querySelector('.navigation__button_main');
+  const aboutBtn = document.querySelector('.navigation__button_about');
+  const projectsBtn = document.querySelector('.navigation__button_projects');
+  const contactsBtn = document.querySelector('.navigation__button_contacts');
+  const topBtn = document.querySelector('.navigation__button_top');
+  const downBtn = document.querySelector('.navigation__button_down');
+  const swiperContainer = document.querySelector('.swiper-container');
+
   if (window.innerWidth > 480) {
     params = {
       simulateTouch: false,
@@ -13,19 +21,25 @@ document.addEventListener('DOMContentLoaded', () => {
     params = {
       direction: 'vertical',
       noSwipingClass: 'swiper-slide',
-      autoHeight: true,
       on: {
         resize: () => mySwiper.changeDirection(getDirection())
       }
     }
   }
-  const mySwiper = new Swiper('.swiper-container', params);
-  const mainBtn = document.querySelector('.navigation__button_main');
-  const aboutBtn = document.querySelector('.navigation__button_about');
-  const projectsBtn = document.querySelector('.navigation__button_projects');
-  const contactsBtn = document.querySelector('.navigation__button_contacts');
-  const topBtn = document.querySelector('.navigation__button_top');
-  const downBtn = document.querySelector('.navigation__button_down');
+
+  // костыль. установка высоты слайдера в зависимости от высоты окна
+  window.addEventListener('resize', () => {
+    if(window.innerWidth <= 480) {
+      swiperContainer.style.height = window.innerHeight - 76 + 'px';
+    } else {
+      swiperContainer.style.height = '';
+    }
+  });
+  if(window.innerWidth <= 480) {
+    swiperContainer.style.height = window.innerHeight - 76 + 'px';
+  };
+
+  const mySwiper = new Swiper(swiperContainer, params);
 
   mainBtn.onclick = () => mySwiper.slideTo(0);
   aboutBtn.onclick = () => mySwiper.slideTo(1);
